54e711c7a163167ca8824bdf97dc032574275d69
rathataaa <jepperath@gmail.com>
2026-04-27 17:40:22 +0200

    Healthbar

---
+++ Assets/Scripts/Handlers/HealthManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthImage;
+
+    public Sprite health100;
+    public Sprite health75;
+    public Sprite health50;
+    public Sprite health25;
+    public Sprite health0;
+
+    private int currentHealth = 100;
+
+    void Start()
+    {
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, 100);
+
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        if (currentHealth > 75)
+            healthImage.sprite = health100;
+        else if (currentHealth > 50)
+            healthImage.sprite = health75;
+        else if (currentHealth > 25)
+            healthImage.sprite = health50;
+        else if (currentHealth > 0)
+            healthImage.sprite = health25;
+        else
+            healthImage.sprite = health0;
+    }
+}
+
